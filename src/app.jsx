ee7f5ae78313e5ddaada2f2b3da2366c7313e918
karan singh D <ceitkaransingh2026@gmail.com>
2024-10-24 23:17:34 +0530

    Added the routes functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,23 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
 import About from "./components/About";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Hero from './components/Hero';
+
+
 
 const App = () => {
   return (
     <Router>
-      <div className="text-white">
-        <Routes>
-          <Route path="/" element={<Body />} />
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route path="/hero" element={<Hero />} />
           <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+        </Route>
+      </Routes>
     </Router>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<App />)
---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import batmanLogo from "../assets/images/batman-logo.png";
+
+const Hero = () => {
+  return (
+    <div className="relative h-screen flex">
+      {/* Left Side - Gym Info Section */}
+      <div className="flex-1 flex items-center justify-start p-12 ml-10">
+        <div className="text-white">
+          <h2 className="text-3xl font-bold">Join AMyeS FiT</h2>
+          <p className="text-lg mt-2">
+            Get fit and strong with our comprehensive workout programs and nutrition plans!
+          </p>
+          <p className="text-lg mt-2">Sign up today and transform your fitness journey!</p>
+        </div>
+      </div>
+      {/* Logo Section */}
+      <div className="h-full flex items-center justify-center mr-14 pr-4 border-double">
+        <img src={batmanLogo} className="h-64" alt="Batman Logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
