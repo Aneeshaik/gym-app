2b457cf0f429fc5285dcd447ec554f798207372c
Akshath P <akshathpkk@gmail.com>
2024-10-26 15:27:01 +0530

    added workout route

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import Body from "./components/Body";
 import About from "./components/About";
+import Workouts from "./components/Workouts";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Body />} />
           <Route path="/about" element={<About />} />
+          <Route path="/workouts" element={<Workouts />} />
         </Routes>
       </div>
     </Router>
